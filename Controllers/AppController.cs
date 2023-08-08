8bd72fb486ddb2bde0891c5216efbf45afea3f44
karaokesound <karaokesound24@gmail.com>
2023-08-08 09:00:59 +0200

    Add project files.

---
+++ Controllers/AppController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BattleshipGame.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new InvalidProgramException("Test");
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,38 @@
+using BattleshipGame.Controllers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BattleshipGame
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Default", "/{controller}/{action}/{id?}",
+                  new { Controller = "App", action = "Index" });
+            });
+        }
+    }
+}
